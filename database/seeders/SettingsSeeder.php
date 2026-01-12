f7a37fffa2a815ee7afb206bb2bff75bcaba9dd9
Vaezy <boy1.thoomas@gmail.com>
2026-01-12 12:10:29 +0100

    chore: init project

---
+++ database/seeders/SettingsSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Settings;
+
+class SettingsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::where('email', 'test@example.com')->first();
+        $user->settings()->create([
+            'theme' => 'dark',
+            'lang'  => 'fr'
+        ]);
+    }
+}
