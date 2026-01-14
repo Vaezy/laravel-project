9e71850942904a87fcc7c42cea53d022d5ebd611
Vaezy <boy1.thoomas@gmail.com>
2026-01-14 11:09:34 +0100

    feat: add activity 5

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/products', [ProductController::class, 'index'])->name('products.index');
 });
